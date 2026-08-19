2d1c3c05e00099f3d888420bfa3ef7dc4eb1ec10
adisuardikayasamade-web <adisuardikayasamade@gmail.com>
2026-08-19 13:58:40 +0800

    Delete components directory

--- components/WAButton.tsx
+++
@@ -1,6 +0,0 @@
-
-import { brand } from "@/lib/data";
-export default function WAButton({ message, children, className="" }: { message: string; children: React.ReactNode; className?: string }) {
-  const url = `https://wa.me/${brand.waNumber}?text=${encodeURIComponent(message)}`;
-  return <a href={url} target="_blank" className={className}>{children}</a>
-}
